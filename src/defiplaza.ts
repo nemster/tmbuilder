573a8c1f2714b535069b409f4412bb57bb85b972
nemster <>
2023-12-01 20:07:59 +0100

    update contracts constants

--- src/defiplaza.ts
+++ src/defiplaza.ts
@@ -1,8 +1,12 @@
 export const defiplaza_listed_coins: {[key: string]: string}= {
   "resource_rdx1thyqj6yn4rmc8awxqhz6d60q9t677tre0ervrqpzfp4fzc20s4cmce": "dfp2 DefiPlaza",
-  "resource_rdx1tkmwanknxau3f62kvufsk7sn80gaqhtllz8dasuq2en7nzdfka4t2t": "ida XIDAR",
-  "resource_rdx1tkm7e4gw7v08qcy4f7f46acr06mycqhz0ppr7dyshjxvcxmegj7du7": "oci Ociswap",
-  "resource_rdx1tkhseye4w0hmf2af5enwurkxu4x29zk73yckyzhndv8xdk8tp2tn8q": "floop Floop",
-  "resource_rdx1t5ga7j04ek4laprf7xryxlu4hl04373sskdaaptfnjhnsns98vma8m": "astrl Astrolescent",
   "resource_rdx1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxradxrd": "XRD Radix",
+  "resource_rdx1t4tjx4g3qzd98nayqxm7qdpj0a0u8ns6a0jrchq49dyfevgh6u0gj3": "ASTRL Astrolescent(Babylon)",
+  "resource_rdx1t52pvtk5wfhltchwh3rkzls2x0r98fw9cjhpyrf3vsykhkuwrf7jg8": "OCI Ociswap",
+  "resource_rdx1t5pyvlaas0ljxy0wytm5gvyamyv896m69njqdmm2stukr3xexc2up9": "FLOOP Floop",
+  "resource_rdx1t4km4k306ul40s3zr8zwwrm25xfmx7w8ytjvdwqh0u3kkch0eph9rn": "foton Foton",
+  "resource_rdx1t4qfgjm35dkwdrpzl3d8pc053uw9v4pj5wfek0ffuzsp73evye6wu6": "dgc Doge³",
+  "resource_rdx1t43srxdw79mlz6xa74sav09ke92jfyf8mnm3q9z64kdjh3tnhye9c0": "dgulden DigiGulden",
+  "resource_rdx1tk2ekrvckgptrtls6zp0uautg8t34nzl3h93vagt66k49vh757w5px": "DPH Delphibets",
+  "resource_rdx1tk3fxrz75ghllrqhyq8e574rkf4lsq2x5a0vegxwlh3defv225cth3": "WEFT WeftFinance",
 };
