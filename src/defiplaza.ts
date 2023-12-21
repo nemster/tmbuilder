b1845475b6aadca0ad4f3c0adef93446ef8c0e55
nemster <>
2023-12-21 16:06:31 +0100

    update constants

--- src/defiplaza.ts
+++ src/defiplaza.ts
@@ -1,7 +1,7 @@
 export const defiplaza_listed_coins: {[key: string]: string}= {
   "resource_rdx1thyqj6yn4rmc8awxqhz6d60q9t677tre0ervrqpzfp4fzc20s4cmce": "dfp2 DefiPlaza",
   "resource_rdx1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxradxrd": "XRD Radix",
-  "resource_rdx1t4tjx4g3qzd98nayqxm7qdpj0a0u8ns6a0jrchq49dyfevgh6u0gj3": "ASTRL Astrolescent(Babylon)",
+  "resource_rdx1t4tjx4g3qzd98nayqxm7qdpj0a0u8ns6a0jrchq49dyfevgh6u0gj3": "Astrolescent ASTRL",
   "resource_rdx1t52pvtk5wfhltchwh3rkzls2x0r98fw9cjhpyrf3vsykhkuwrf7jg8": "OCI Ociswap",
   "resource_rdx1t5pyvlaas0ljxy0wytm5gvyamyv896m69njqdmm2stukr3xexc2up9": "FLOOP Floop",
   "resource_rdx1t4km4k306ul40s3zr8zwwrm25xfmx7w8ytjvdwqh0u3kkch0eph9rn": "foton Foton",
@@ -9,4 +9,7 @@ export const defiplaza_listed_coins: {[key: string]: string}= {
   "resource_rdx1t43srxdw79mlz6xa74sav09ke92jfyf8mnm3q9z64kdjh3tnhye9c0": "dgulden DigiGulden",
   "resource_rdx1tk2ekrvckgptrtls6zp0uautg8t34nzl3h93vagt66k49vh757w5px": "DPH Delphibets",
   "resource_rdx1tk3fxrz75ghllrqhyq8e574rkf4lsq2x5a0vegxwlh3defv225cth3": "WEFT WeftFinance",
+  "resource_rdx1tknu3dqlkysz9lt08s7spuvllz3kk2k2yccslfpdk73t4lnznl9jck": "GAB GAB",
+  "resource_rdx1t4zrksrzh7ucny7r57ss99nsrxscqwh8crjn6k22m8e9qyxh8c05pl": "rdk Radket",
+  "resource_rdx1thn6xa5vjdh5zagqzvxkxpd70r6eadpzmzr83m20ayp3yhxrjavxz5": "PLANET RadixPlanetTokenV2",
 };
